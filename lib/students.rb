30dabaee050b9e6404342d4e4a1409c774b907f6
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-12 22:12:46 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,15 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  " SELECT MIN(gpa) FROM students"
+  "SELECT MIN(gpa) FROM students"
 end
 
 def average_student_gpa
+    "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+    "SELECT (gpa) FROM students"
 end
 
 def average_gpa_for_9th_grade
